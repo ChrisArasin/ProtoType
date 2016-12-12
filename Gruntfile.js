5ed5f2709ea27db0093fa4413d97212defa523b2
ChrisArasin <chris.arasin@gmail.com>
2016-12-12 15:33:39 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+	var themeName = 'theme-name';
+	var outFile = 'public/' + themeName + '-bundle.css';
+	var inFile = 'sass/' + themeName + '.scss'
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+        options: {
+          style: 'compressed'
+        },
+				files: {
+					[outFile] : inFile
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ProtoType
+A small css framework for type-focused layouts.
+
+# Getting Started
+In the /sass/ folder, duplicate main.scss or one of the sample theme files. Then, in the Gruntfile, update the themeName variable to the name of your new root scss file. Run grunt watch and have fun designing editorial layouts in the browser.
+
+Sample images from https://unsplash.com/
