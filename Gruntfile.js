ca4951d8280a1e3cd316d4036a913ebbed55017d
ChrisArasin <chris.arasin@gmail.com>
2017-01-20 15:57:21 -0500

    clean gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,26 @@ module.exports = function(grunt) {
   var themeName = 'simple';
   var outFile = 'public/' + themeName + '-bundle.css';
   var inFile = 'sass/' + themeName + '.scss'
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-			sass: {
-			dist: {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
         options: {
           style: 'compressed'
         },
-				files: {
-					[outFile] : inFile
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+        files: {
+          [outFile] : inFile
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
 }
